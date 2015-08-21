3e518ab3cd6fe8548f7b2901c26c780a5abb5edd
Khemmachart <nueve.sr@gmail.com>
2015-08-21 17:29:11 +0700

    Added QuestionViewController .h/m

---
+++ singlequiz/QuestionViewControllers.h
@@ -0,0 +1,13 @@
+//
+//  QuestionViewController.h
+//  singlequiz
+//
+//  Created by KHUN NINE on 8/21/15.
+//  Copyright (c) 2015 KHUN NINE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionViewControllers : UIViewController
+
+@end
